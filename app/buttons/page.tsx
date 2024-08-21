1f5dd8bee62628cc155573270f74daf18c73bb70
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-21 14:23:11 +0300

    style primaryOutline variant

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -5,7 +5,10 @@ const ButtonsPage = ()=>{
         <div className="p-4 space-y-4 flex flex-col w-2/5 max-w-[200px]">
             <Button>Default</Button>
             <Button variant="primary">Primary</Button>
-            <Button>Primary Outline</Button>
+            <Button variant="primaryOutline">Primary Outline</Button>
+
+            <Button variant="primary">Secondary</Button>
+            <Button variant="primaryOutline">Secondary Outline</Button>
         </div>
 
     );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
       variant: {
         default: "bg-white text-black border-slate-200 border-2 border-b-4 active:border-b-2 hover:bg-slate-100 text-slate-500",
         primary: "bg-sky-400 text-primary-foreground hover:ng-sky-400/90 border-sky-500 border-b-4 active:border-b-0",
-        primaryOutline: ""
+        primaryOutline: "bg-white text-sky-500 hover:bg-slate-100"
       },
       size: {
         default: "h-11 px-4 py-2",
