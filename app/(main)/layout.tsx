d6dfcd3a132c5857108503869c6e304b27bbb180
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-22 15:59:39 +0300

    set mobile header

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,13 +1,14 @@
 import { Sidebar } from "@/components/sidebar";
-
+import { MobileHeader } from "@/components/mobileHeader";
 type Props = {
     children: React.ReactNode;
 }
 const MainLayout = ({children}:Props)=>{
     return(
         <>
-        <Sidebar className="hidden lg:flex"></Sidebar>
-        <main className="lg:pl-[256px] h-full">
+        <MobileHeader/>
+        <Sidebar className="hidden lg:flex"/>
+        <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
             <div className="bg-red-500 h-full">
             {children}
             </div>
