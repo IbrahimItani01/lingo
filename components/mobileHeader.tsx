9c68a979bde734bf900bb1148e1dc2ab449bdca8
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-22 16:12:23 +0300

    change mobile header bg color

--- components/mobileHeader.tsx
+++ components/mobileHeader.tsx
@@ -1,7 +1,7 @@
 import { MobileSidebar } from "./mobileSidebar";
 export const MobileHeader=()=>{
     return(
-        <nav className="px-4 h-[50px] flex items-center bg-yellow-500 border-b fixed top-0 w-full z-50 lg:hidden">
+        <nav className="px-4 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50 lg:hidden">
             <MobileSidebar/>
         </nav>
     );
