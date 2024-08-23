faac68d91a729882e9c93b32a937d5e1e2378b14
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-23 15:01:11 +0300

    create the learn page header

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,11 +1,18 @@
 import { StickyWrapper } from "@/components/stickyWrapper";
 import { FeedWrapper } from "@/components/feedWrapper";
 import { Header } from "./header";
+import { UserProgress } from "@/components/userProgress";
 const LearnPage = ()=>{
     return(
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                My sticky sidebar
+                <UserProgress
+                 //pretend asif a course is loaded
+                    activeCourse={{title:"Spanish",imageSrc: "/es.png"}}
+                    hearts={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                ></UserProgress>
             </StickyWrapper>
             <FeedWrapper>
                 <Header title='Spanish'></Header>
