46a6931471cf03bbf9df88a166bc892a260ed719
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-23 13:54:55 +0300

    add logo and learn page redirect button to siddebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,7 @@
 import { cn } from "@/lib/utils";
-
+import Image from "next/image";
+import Link from "next/link";
+import { SidebarItem } from "./sidebarItem";
 type Props ={
     className?:string;
 }
@@ -7,9 +9,19 @@ type Props ={
 export const Sidebar= ({className}:Props)=>{
     return(
         <div className={cn(
-            "bg-blue-500 h-full lg:w-[256px] lg:fixed flex left-0 top-0 px-4 border-r-2 flex-col",className,
+            "h-full lg:w-[256px] lg:fixed flex left-0 top-0 px-4 border-r-2 flex-col",className,
         )}>
-            Sidebar
+            <Link href="/learn">
+                <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+                        <Image src="/logo.png" height={40} width={40} alt="Logo"/>
+                        <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">Lingo</h1>
+                    </div>
+            </Link>
+            <div className="flex flex-col gap-y-2 flex-1">
+                <SidebarItem iconSrc="/learn.png"  label="learn" href="/learn"></SidebarItem>
             </div>
+        </div>
+            
+              
     );
 }
