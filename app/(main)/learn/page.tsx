2230f027cd43ed160d28facab82856c1d3a1441d
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-23 14:18:23 +0300

    build sticky wrapper component

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,6 +1,14 @@
+import { StickyWrapper } from "@/components/stickyWrapper";
 const LearnPage = ()=>{
     return(
-        <div>Learn Page</div>
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                My sticky sidebar
+            </StickyWrapper>
+            <FeedWrapper>
+                My feed
+            </FeedWrapper>
+        </div>
     );
 }
 export default LearnPage;
