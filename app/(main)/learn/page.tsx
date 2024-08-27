4b5a0ed4e2d3e717b5aecdadb325df2b1ea9fc72
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-27 09:42:06 +0300

    "Added getUnits query and integrated it into LearnPage, fetching units for active course and displaying them"

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,11 +2,12 @@ import { StickyWrapper } from "@/components/stickyWrapper";
 import { FeedWrapper } from "@/components/feedWrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/userProgress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 const LearnPage = async ()=>{
     const userProgressData = getUserProgress();
-    const [userProgress] = await Promise.all([userProgressData])
+    const unitsData = getUnits();
+    const [userProgress,units] = await Promise.all([userProgressData,unitsData])
     if(!userProgress || !userProgress.activeCourse){
         redirect("/courses");
     }
@@ -22,6 +23,11 @@ const LearnPage = async ()=>{
             </StickyWrapper>
             <FeedWrapper>
                 <Header title={userProgress.activeCourse.title}></Header>
+                {units.map((unit)=>(
+                    <div key={unit.id} className="mb-10">
+                        {JSON.stringify(unit)}
+                    </div>
+                ))}
             </FeedWrapper>
         </div>
     );
