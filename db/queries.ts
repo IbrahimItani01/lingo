9f072e909ff973df3bfe8f28eba5cecee6a741ca
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-29 14:22:54 +0300

    "Added challengeProgress to schema import, updated getUserProgress and getUnits queries to include userId and challengeProgress"

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { userProgress, courses, units, challengeOptions } from "./schema";
+import { userProgress, courses, units, challengeOptions, challengeProgress } from "./schema";
 import {eq} from "drizzle-orm";
 export const getUserProgress = cache(async ()=>{
     const {userId} = await auth();
@@ -19,10 +19,12 @@ export const getUserProgress = cache(async ()=>{
     
 })
 export const getUnits = cache(async ()=>{
+    const {userId} = await auth();
     const userProgress = await getUserProgress();
-    if(!userProgress?.activeCourseId){
+    if(!userId || !userProgress?.activeCourseId){
         return [];
     }
+    // TODO: Confirm wether order is needed
     const data = await db.query.units.findMany({
         where: eq(units.courseId, userProgress.activeCourseId),
         with: {
@@ -30,8 +32,10 @@ export const getUnits = cache(async ()=>{
                 with:{
                     challenges: {
                         with: {
-                            challengeProgress:true,
-                        }
+                            challengeProgress:{
+                                where: eq(challengeProgress.userId, userId,)
+                            },
+                        },
                     }
                 }
             }
