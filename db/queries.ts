600173c242d435f81bd796c9dcd36b1791a9001b
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-02 13:16:02 +0300

    "Added new `getLesson` function and modified `getCourseProgress` function to include additional condition in challenge progress check"

--- db/queries.ts
+++ db/queries.ts
@@ -98,13 +98,56 @@ export const getCourseProgress = cache(async ()=>{
     const firstUncompletedLesson = unitsInActiveCourse
     .flatMap((unit)=> unit.lessons)
     .find((lesson)=>{
+        //TODO: if something does not work check the last if clause
         return lesson.challenges.some((challenge)=>{
-            return !challenge.challengeProgress || challenge.challengeProgress.length === 0; 
+            return !challenge.challengeProgress || challenge.challengeProgress.length === 0
+            || challenge.challengeProgress.some((progress)=> progress.completed===false); 
         })
     })
     return{
         activeLesson: firstUncompletedLesson,
         activeLessonId: firstUncompletedLesson?.id,
+    }
+})
+
+export const getLesson = cache(async (id?:number)=>{
+    const {userId} = await auth();
+    if(!userId){
+        return null;
+    }
+    const courseProgress = await getCourseProgress();
+
+    const lessonId = id || courseProgress?.activeLessonId;
+    if (!lessonId){
+        return null;
+    }
+    const data = await db.query.lessons.findFirst({
+        where: eq(lessons.id, lessonId),
+        with:{
+            challenges:
+            {
+                orderBy: (challenges,{asc}) => [asc(challenges.order)],
+                with:{
+                    challengeOptions: true,
+                    challengeProgress:{
+                        where: eq(challengeProgress.userId,userId),
+                    }
+                }
+            }
+        }
+    });
+    if (!data || !data.challenges){
+        return null;
+    }
+    const normalizeChallenges = data.challenges.map((challenge)=>{
+        //TODO: if something does not work check the last if clause
+        const completed = challenge.challengeProgress
+         && challenge.challengeProgress.length>0
+         && challenge.challengeProgress.every((progress)=> progress.completed);
+        return {...challenge,completed}
+    });
+    return{
+        ...data,challenges: normalizeChallenges
     }
 })
 
