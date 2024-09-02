7b6a1cfbd678d6d5660d4d65cea6cdc53bf4d102
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-02 13:02:07 +0300

    create getCourseProgress in queries.tsx

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { userProgress, courses, units, challengeOptions, challengeProgress } from "./schema";
+import { userProgress, courses, units, challengeOptions, challengeProgress, lessons } from "./schema";
 import {eq} from "drizzle-orm";
 export const getUserProgress = cache(async ()=>{
     const {userId} = await auth();
@@ -66,5 +66,45 @@ export const getCourseById = cache(async (courseID: number)=>{
         // TODO: Populate units and lessons
     })
     return data;
+})
+
+export const getCourseProgress = cache(async ()=>{
+    const {userId} = await auth();
+    const userProgress = await getUserProgress();
+
+    if (!userId || !userProgress?.activeCourseId)
+    {
+        return null;
+    }
+    const unitsInActiveCourse = await db.query.units.findMany({
+        orderBy: (units,{asc})=> [asc(units.order)],
+        where: eq(units.courseId, userProgress.activeCourseId),
+        with: {
+            lessons:{
+                orderBy:(lessons,{asc}) => [asc(lessons.order)],
+                with:{
+                    unit: true,
+                    challenges:{
+                        with:{
+                            challengeProgress:{
+                                where: eq(challengeProgress.userId,userId),
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    })
+    const firstUncompletedLesson = unitsInActiveCourse
+    .flatMap((unit)=> unit.lessons)
+    .find((lesson)=>{
+        return lesson.challenges.some((challenge)=>{
+            return !challenge.challengeProgress || challenge.challengeProgress.length === 0; 
+        })
+    })
+    return{
+        activeLesson: firstUncompletedLesson,
+        activeLessonId: firstUncompletedLesson?.id,
+    }
 })
 
