0a6ed45645c03018b46a16edc0a955b0b50c9023
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-05 12:31:41 +0300

    prepare lesson header components and layout.tsx

---
+++ app/lesson/header.tsx
@@ -0,0 +1,10 @@
+type Props = {
+    hearts: number;
+    percentage: number;
+    hasActiveSubscription: boolean;
+}
+export const Header =({hearts,percentage,hasActiveSubscription}:Props)=>{
+    return(
+        <header></header>
+    )
+}
