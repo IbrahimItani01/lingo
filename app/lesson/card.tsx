b2c625060692017515cef800e0e9db2e578f4311
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-07 17:14:43 +0300

    Add audio files and integrate in card.tsx

--- app/lesson/card.tsx
+++ app/lesson/card.tsx
@@ -1,7 +1,9 @@
 import { challenges } from "@/db/schema";
 import { cn } from "@/lib/utils";
+import { Handshake } from "lucide-react";
 import Image from "next/image";
-
+import { useCallback } from "react";
+import {useAudio, useKey} from "react-use";
 type Props={
     id:number;
     text: string;
@@ -17,9 +19,17 @@ type Props={
 export const Card = ({
     id,imageSrc,audioSrc,text,shortcut,selected,onClick,status,disabled,type,
 }:Props)=>{
+    const [audio,_,controls] = useAudio({src:audioSrc || ""});
+    const handleClick = useCallback(()=>{
+        if(disabled) return;
+        controls.play();
+        onClick();
+    },[disabled,onClick,controls])
+
+    useKey(shortcut, handleClick,{},[handleClick]);
     return(
         <div
-            onClick={()=> {}}
+            onClick={handleClick}
             className={
                 cn(
                     "h-full border-2 rounded-xl border-b-4 hover:bg-black/5 p-4 lg:p-6 cursor-pointer active:border-b-2",
@@ -30,6 +40,7 @@ export const Card = ({
                     type === "ASSIST" && "lg:p-3 w-full"
                 )}
         >
+            {audio}
             {imageSrc && (
                 <div className="relative aspect-square mb-4 max-h-[90px] lg:max-h-[150px] w-full">
                     <Image
