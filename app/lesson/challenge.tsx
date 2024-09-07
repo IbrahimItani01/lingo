7f6decfb5e3b32fb8f4ffc278db202c1a698ceca
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-07 14:52:53 +0300

    "Added utility function 'cn' and refactored Challenge component to use it for conditional class names, and updated Card component props."

--- app/lesson/challenge.tsx
+++ app/lesson/challenge.tsx
@@ -1,4 +1,5 @@
 import { challengeOptions, challenges } from "@/db/schema"
+import { cn } from "@/lib/utils";
 
 type Props = {
     options:typeof challengeOptions.$inferSelect[];
@@ -10,8 +11,26 @@ type Props = {
 };
 export const Challenge = ({options,onSelect,status,selectedOption,disabled,type}:Props)=>{
     return(
-        <div>
-            Challenge
+        <div className={cn(
+            "grid gap-2",
+            type === "ASSIST" && "grid-cols-1",
+            type ==="SELECT" && "grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(0,1fr))]",
+        )}>
+            {options.map((option,i)=>(
+                <Card
+                    key={option.id}
+                    id={option.id}
+                    text={option.text}
+                    imageSrc={option.imageSrc}
+                    shortcut={`${i+1}`}
+                    selected={selectedOption===option.id}
+                    onClick={()=>onSelect(option.id)}
+                    status={status}
+                    audioSrc={option.audioSrc}
+                    disabled={disabled}
+                    type={type}
+                />
+            ))}
         </div>
     );
 }
