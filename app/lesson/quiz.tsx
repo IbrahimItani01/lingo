4c14052174a99699e200faea4b8ab2a38708fa30
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-08 16:53:50 +0300

    "Added new action to reduce hearts, updated quiz component with new UI and logic, and seeded new challenges and options in the database."

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { challengeOptions, challenges } from "@/db/schema";
-import { useState } from "react";
+import { useState,useTransition } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./questionBubble";
 import { Challenge } from "./challenge";
 import { Footer } from "./footer";
+import { upsertChallengeProgress } from "@/actions/challenge-progress";
+import { toast } from "sonner";
 type Props ={
     initialPercentage: number;
     initialHearts: number;
@@ -17,6 +19,9 @@ type Props ={
     userSubscription: any;
 }
 export const Quiz = ({initialHearts,initialLessonChallenges,initialLessonId,initialPercentage,userSubscription}: Props)=>{
+    const [pending,startTransition] = useTransition();
+
+    
     const [hearts,setHearts]= useState(initialHearts);
     const [percentage,setPercentage]=useState(initialPercentage);
     const [challenges] = useState(initialLessonChallenges);
@@ -29,10 +34,55 @@ export const Quiz = ({initialHearts,initialLessonChallenges,initialLessonId,init
     const challenge = challenges[activeIndex];
     const options = challenge?.challengeOptions ?? [];
 
+    const onNext = ()=>{
+        setActiveIndex((current) => current+1);
+    }
+
     const onSelect = (id:number)=>{
         if(status !== "none") return;
         setSelectedOption(id);
     }
+
+    const onContinue = ()=>{
+        if(!selectedOption) return;
+        if(status === "wrong"){
+            setStatus("none");
+            setSelectedOption(undefined);
+            return;
+        }
+        if(status === "correct"){
+            onNext();
+            setStatus("none");
+            setSelectedOption(undefined);
+            return;
+        }
+        const correctOption = options.find((option)=>option.correct);
+
+        if(!correctOption) return;
+        
+
+        if(correctOption && correctOption.id === selectedOption){
+            startTransition(()=>{
+                upsertChallengeProgress(challenge.id)
+                .then((response)=>{
+                    if(response?.error === 'hearts'){
+                        console.error("Missing hearts");
+                        return;
+                    }
+                    setStatus("correct");
+                    setPercentage((prev)=>prev+100/challenges.length);
+
+                    if(initialPercentage===100){
+                        setHearts((prev)=>Math.min(prev+1,5))
+                    }
+                }).catch(()=>toast.error("Something went wrong. Please try again."))
+                
+            })
+        }else{
+            console.error("Incorrect option!");
+        }
+
+    };
 
     const title = challenge.type === "ASSIST"? "Select the correct Meaning": challenge.question;
     return(
@@ -57,7 +107,7 @@ export const Quiz = ({initialHearts,initialLessonChallenges,initialLessonId,init
                                 onSelect={onSelect}
                                 status = {status}
                                 selectedOption ={selectedOption}
-                                disabled={false}
+                                disabled={pending}
                                 type= {challenge.type}
                              />
                         </div>
@@ -65,9 +115,9 @@ export const Quiz = ({initialHearts,initialLessonChallenges,initialLessonId,init
                 </div>
             </div>
             <Footer
-                disabled={!selectedOption}
+                disabled={pending || !selectedOption}
                 status={status}
-                onCheck={()=>{}}
+                onCheck={onContinue}
             />
         </>
     );
--- scripts/seed.ts
+++ scripts/seed.ts
@@ -93,10 +93,23 @@ const main = async ()=>{
               order: 1,
               question: 'Which one of these is the "the man"?',
             },
+            {
+                id: 2,
+                lessonId: 1,
+                type: "ASSIST",
+                order: 2,
+                question: '"the man"',
+            },
+            {
+                id: 3,
+                lessonId: 1,
+                type: "SELECT",
+                order: 3,
+                question: 'Which one of these is the "the robot"?',
+            },
         ]);
         await db.insert(schema.challengeOptions).values([
             {
-                id: 1,
                 challengeId: 1,
                 imageSrc: "/man.png",
                 correct: true,
@@ -104,7 +117,6 @@ const main = async ()=>{
                 audioSrc: "/es_man.mp3",
             },
             {
-                id: 2,
                 challengeId: 1,
                 imageSrc: "/woman.png",
                 correct: false,
@@ -112,10 +124,53 @@ const main = async ()=>{
                 audioSrc: "/es_woman.mp3",
             },
             {
-                id: 3,
                 challengeId: 1,
                 imageSrc: "/robot.png",
                 correct: false,
+                text: "el robot",
+                audioSrc: "/es_robot.mp3",
+            },
+        ])
+
+        await db.insert(schema.challengeOptions).values([
+            {
+                challengeId: 2,
+                correct: true,
+                text: "el hombre",
+                audioSrc: "/es_man.mp3",
+            },
+            {
+                challengeId: 2,
+                correct: false,
+                text: "el mujer",
+                audioSrc: "/es_woman.mp3",
+            },
+            {
+                challengeId: 2,
+                correct: false,
+                text: "el robot",
+                audioSrc: "/es_robot.mp3",
+            },
+        ])
+        await db.insert(schema.challengeOptions).values([
+            {
+                challengeId: 3,
+                imageSrc: "/man.png",
+                correct: false,
+                text: "el hombre",
+                audioSrc: "/es_man.mp3",
+            },
+            {
+                challengeId: 3,
+                imageSrc: "/woman.png",
+                correct: false,
+                text: "el mujer",
+                audioSrc: "/es_woman.mp3",
+            },
+            {
+                challengeId: 3,
+                imageSrc: "/robot.png",
+                correct: true,
                 text: "el robot",
                 audioSrc: "/es_robot.mp3",
             },
