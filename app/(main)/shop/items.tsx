2e91a3f7e399e85c67ce4c0b0438d7973a430894
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-10 14:01:20 +0300

    design store refill hearts button behavior

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,5 +1,8 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+const pointsToRefill = 10;
 type Props = {
     hearts: number;
     points: number;
@@ -8,8 +11,37 @@ type Props = {
 
 export const Items = ({hearts,points,hasActiveSubscription}:Props)=>
 {
-    
+
     return(
-        <div></div>
+        <ul className="w-full">
+            <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
+                <Image
+                    src="/heart.png"
+                    alt="heart"
+                    height={60}
+                    width={60}
+                />
+                <div className="flex-1">
+                    <p className="text-neutral-700 text-base lg:text-xl font-bold">
+                        Refill Hearts 
+                    </p>
+                </div>
+                <Button
+                    disabled={hearts===5 || points < pointsToRefill}
+                >
+                    {hearts ===5 ? "full": (
+                        <div className="flex items-center">
+                            <Image
+                                src="/points.png"
+                                alt="points"
+                                width={20}
+                                height={20}
+                            />
+                            <p>{pointsToRefill}</p>
+                        </div>
+                    )}
+                </Button>
+            </div>
+        </ul>
     );
 }
