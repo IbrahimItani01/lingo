c3eedbd96907f897132c661758b9932c07da4802
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-10 13:42:16 +0300

    add shop route and page.tsx

---
+++ app/(main)/shop/items.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+type Props = {
+    hearts: number;
+    points: number;
+    hasActiveSubscription: boolean;
+}
+
+export const Items = ({hearts,points,hasActiveSubscription}:Props)=>
+{
+    
+    return(
+        <div></div>
+    );
+}
