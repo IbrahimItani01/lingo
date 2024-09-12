5657b25dbe801611091f30cbd0535c192e19723e
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-12 21:21:20 +0300

    "Updated clerkMiddleware to include createRouteMatcher and conditional auth protection"

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,11 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/","/api/webhooks/stripe"])
 
+export default clerkMiddleware((auth,req)=>{
+  if(isPublicRoute(req)) return
+  auth().protect()
+});
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
