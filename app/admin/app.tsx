183c87af5530032b76435e41a2cb5db331b344a1
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-16 17:38:17 +0300

    test first resource for courses

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -1,12 +1,16 @@
 "use client";
-import {Admin} from "react-admin";
+import {Admin, ListGuesser, Resource} from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
 
 const dataProvider= simpleRestProvider("/api");
 const App=()=>{
     return(
     <Admin dataProvider={dataProvider}>
-        Admin
+        <Resource
+            name="courses"
+            recordRepresentation="title"
+            list={ListGuesser}
+        />
     </Admin>
     );
 };
