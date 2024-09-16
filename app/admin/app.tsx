ae2d5a9481516a62c1c05b9ff2ec5771feb8b833
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-16 17:35:37 +0300

    add data provider to the admin

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -1,8 +1,11 @@
 "use client";
 import {Admin} from "react-admin";
+import simpleRestProvider from "ra-data-simple-rest";
+
+const dataProvider= simpleRestProvider("/api");
 const App=()=>{
     return(
-    <Admin>
+    <Admin dataProvider={dataProvider}>
         Admin
     </Admin>
     );
