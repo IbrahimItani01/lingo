eccce6f9af7928b55c925f5cdadac66526ef9959
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-17 15:33:07 +0300

    create course list edit

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,8 +1,12 @@
 import { NextResponse } from "next/server";
 import { courses } from "@/db/schema";
 import db from "@/db/drizzle";
+import { isAdmin } from "@/lib/auth";
 
 export const GET = async ()=>{
+    if(!isAdmin()){
+        return new NextResponse("Unauthorized",{status:401});
+    }
     const data = await db.query.courses.findMany();
     return NextResponse.json(data);
 }
