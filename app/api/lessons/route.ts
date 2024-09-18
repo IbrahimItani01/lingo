377dc2140452e05617118ad848e4841b7430f6e2
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-18 15:05:36 +0300

    create api route fo lessons

---
+++ app/api/lessons/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { lessons } from "@/db/schema";
+import db from "@/db/drizzle";
+import { isAdmin } from "@/lib/auth";
+
+export const GET = async ()=>{
+    if(!isAdmin()){
+        return new NextResponse("Unauthorized",{status:401});
+    }
+    const data = await db.query.lessons.findMany();
+    return NextResponse.json(data);
+}
+export const POST = async (req: Request)=>{
+    if(!isAdmin()){
+        return new NextResponse("Unauthorized",{status:401});
+    }
+    const body = await req.json();
+    const data = await db.insert(lessons).values({
+        ...body,
+    }).returning();
+    return NextResponse.json(data[0]);
+}
