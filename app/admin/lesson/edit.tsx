e9ab61e768acda7482061fdf5f00294bd1faa6d5
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-18 15:08:57 +0300

    integrate the edit method in admin dashboard

--- app/admin/lesson/edit.tsx
+++ app/admin/lesson/edit.tsx
@@ -1,14 +1,12 @@
 import { SimpleForm, TextInput,Edit,required, ReferenceInput, NumberInput } from "react-admin";
-export const UnitEdit = ()=>{
+export const LessonEdit = ()=>{
     return(
         <Edit>
            <SimpleForm>
-                <NumberInput source="id" validate={[required()]} label="Id" />
                 <TextInput source="title" validate={[required()]} label="Title" />
-                <TextInput source="description" validate={[required()]} label="Description" />
                 <ReferenceInput 
-                    source="courseId"
-                    reference="courses"
+                    source="unitId"
+                    reference="units"
                 />
                 <NumberInput
                     source="order"
