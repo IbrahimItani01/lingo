9d431d9a61e972605a130f403126db6ba154ff37
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-18 14:48:33 +0300

    edit units api route

--- app/api/units/[unitId]/route.ts
+++ app/api/units/[unitId]/route.ts
@@ -1,21 +1,21 @@
 import db from "@/db/drizzle"
-import { courses } from "@/db/schema"
+import { units } from "@/db/schema"
 import { isAdmin } from "@/lib/auth";
 import { eq } from "drizzle-orm"
 import { NextResponse } from "next/server";
 
-export const GET = async (req: Request,{params}:{params:{courseId:number}})=>{
+export const GET = async (req: Request,{params}:{params:{unitId:number}})=>{
     
     if(!isAdmin()){
         return new NextResponse("Unauthorized",{status: 403})
     }
     
-    const data = await db.query.courses.findFirst({
-        where: eq(courses.id,params.courseId),
+    const data = await db.query.units.findFirst({
+        where: eq(units.id,params.unitId),
     });
     return NextResponse.json(data);
 }
-export const PUT = async (req: Request,{params}:{params:{courseId:number}})=>{
+export const PUT = async (req: Request,{params}:{params:{unitId:number}})=>{
     
     if(!isAdmin()){
         return new NextResponse("Unauthorized",{status: 403})
@@ -27,32 +27,32 @@ export const PUT = async (req: Request,{params}:{params:{courseId:number}})=>{
         const { id, ...updatedBody } = body;
 
         // Update the other fields first
-        const data = await db.update(courses)
+        const data = await db.update(units)
             .set({ ...updatedBody })
-            .where(eq(courses.id, params.courseId))
+            .where(eq(units.id, params.unitId))
             .returning();
 
         // Now update the id, if needed
-        await db.update(courses)
+        await db.update(units)
             .set({ id: body.id })
-            .where(eq(courses.id, params.courseId));
+            .where(eq(units.id, params.unitId));
 
         return NextResponse.json({ ...data[0], id: body.id });
     } 
     // Normal update if id is not part of the update
-    const data = await db.update(courses)
+    const data = await db.update(units)
         .set({ ...body })
-        .where(eq(courses.id, params.courseId))
+        .where(eq(units.id, params.unitId))
         .returning();
 
     return NextResponse.json(data[0]);
  
 }
-export const DELETE = async (req: Request,{params}:{params:{courseId:number}})=>{
+export const DELETE = async (req: Request,{params}:{params:{unitId:number}})=>{
     
     if(!isAdmin()){
         return new NextResponse("Unauthorized",{status: 403})
     }
-    const data = await db.delete(courses).where(eq(courses.id,params.courseId)).returning();
+    const data = await db.delete(units).where(eq(units.id,params.unitId)).returning();
     return NextResponse.json(data[0]);
 }
