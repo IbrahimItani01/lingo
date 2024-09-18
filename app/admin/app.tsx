b33e56407646e08e0a70b5e4eebb5ead6c8f90a3
Ibrahim Itani <I.itani2002@gmail.com>
2024-09-18 12:52:56 +0300

    create GET method to edit courses data in admin dashboard

--- app/admin/app.tsx
+++ app/admin/app.tsx
@@ -3,6 +3,7 @@ import {Admin, ListGuesser, Resource} from "react-admin";
 import simpleRestProvider from "ra-data-simple-rest";
 import { CourseList } from "./course/list";
 import { CourseCreate } from "./course/create";
+import { CourseEdit } from "./course/edit";
 
 const dataProvider= simpleRestProvider("/api");
 const App=()=>{
@@ -12,6 +13,7 @@ const App=()=>{
             name="courses"
             recordRepresentation="title"
             create={CourseCreate}
+            edit= {CourseEdit}
             list={CourseList}
         />
     </Admin>
